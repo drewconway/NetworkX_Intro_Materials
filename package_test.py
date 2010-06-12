e946f0c78a45e7d5d88f2224fb608cc077bf8607
Aric Hagberg <aric.hagberg@gmail.com>
2010-06-12 17:33:20 -0600

    Add versions to package testing.

--- package_test.py
+++ package_test.py
@@ -14,36 +14,44 @@ Copyright (c) 2010, under the Simplified BSD License.
 For more information on FreeBSD see: http://www.opensource.org/licenses/bsd-license.php
 All rights reserved.
 """
+required={'networkx':'1.1',
+          'matplotlib':'0.99.0',
+          'numpy':'1.3.0',
+          'scipy':'0.7.0'}
 
-import sys
-import os
+optional={'nose':'0.11.1',
+          'pyparsing':'1.5.0',
+          'pydot':'1.0.2',
+          'foo':'1.0'}
 
-def test_packs(pack_list):
-    """Tests to make sure you have all software for
-    NetworkX workshop"""
-    missing=list()
-    for p in pack_list:
+def test_packs(package_list):
+    """Tests to make sure you have all software for NetworkX workshop"""
+    for p,v in package_list.items():
         try:
             exec("import "+p+" as T")
             ver=T.__version__
-            print "You have "+p+" version "+ver+" installed!"
-        except ImportError:
-            missing.append(p)
-    print ""
-    if len(missing)<1:
-        print "#########  YOU HAVE ALL PACKAGES INSTALLED! ######### "
-    else:
-        print "#########    YOU ARE MISSING THE FOLLOWING PACKAGES    #########"
-        for m in missing:
-            print m
-        
-            
-def main():
-    # PACKAGES
-    packs=['networkx','matplotlib','numpy','scipy','pygraphviz','nose','cjson']
-    # TEST PACK INSTALLATION
-    test_packs(packs)
+            want=v.split('.')
+            need=ver.split('.')
+            versionok=True
+            for w,n in zip(want,need):
+                if w.isdigit() and n.isdigit():
+                    if int(w)>int(n):
+                        versionok=False
+            if versionok:
+                print "%12s-%-8s [installed] version [OK] %s"%(p,v,ver)
+            else:
+                print "%12s-%-8s [installed] version [NOTOK] %s"%(p,v,ver)
 
+        except ImportError:
+            print "%12s-%-8s [missing]"%(p,v)
+        
 if __name__ == '__main__':
-    main()
+    print "Required"
+    print "--------"
+    test_packs(required)
+    print
+    print "Optional"
+    print "--------"
+    test_packs(optional)
+
 
